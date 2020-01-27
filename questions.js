0178617c2774a2860d6dd653c3a0e875e7990d74
Max Kennedy <maxken512@gmail.com>
2020-01-26 23:04:55 -0500

    questions list and index page

---
+++ questions.js
@@ -0,0 +1,53 @@
+var questions = [ {
+    "question" : "Who won Thrasher Magazine's skater of the year in 2019?",
+    "optionA" : "Marc Suciu",
+    "optionB" : "Carlos Ribiero",
+    "optionC" : "Milton Martinez",
+    "optionD" : "Thiago Lemos",
+    "answer" : "c"
+}, {
+    "question": "Who was the youngest skateboarder to win a gold medal at the X-Games?",
+    "optionA" : "Ryan Sheckler",
+    "optionB" : "Nyjah Huston",
+    "optionC" : "Paul Rodriguez",
+    "optionD" : "Ishod Wair",
+    "answer" : "a"
+}, {
+    "question" : "Who is the winningest street skateboarder of all time?",
+    "optionA" : "Chris Cole",
+    "optionB" : "Nyjah Huston",
+    "optionC" : "Paul Rodriguez",
+    "optionD" : "Shane O'Neill",
+    "answer" : "b"
+}, {
+    "question" : "Who was the first skateboarder to do a 900 degree rotation in mid air?",
+    "optionA" : "Bob Burnquist",
+    "optionB" : "Tony Hawk",
+    "optionC" : "Shaun White",
+    "optionD" : "Bucky Lasek",
+    "answer" : "b"
+}, {
+    "question" : "What classic Nike basketball silhoutte was the first 'SB'(skateboarding) shoe?",
+    "optionA" : "Air Jordan",
+    "optionB" : "Air Max",
+    "optionC" : "Dunk Low",
+    "optionD" : "Dunk High",
+    "answer" : "c"
+}, {
+    "question" : "When was the first Thrasher Magazine published?",
+    "optionA" : "1976",
+    "optionB" : "1990",
+    "optionC" : "1987",
+    "optionD" : "1981",
+    "answer" : "d"
+}, {
+    "question" : "What skateboarder invented the trick 'the impossible'?",
+    "optionA" : "Rodney Mullen",
+    "optionB" : "Daewon Song",
+    "optionC" : "Tony Hawk",
+    "optionD" : "Bucky Lasek",
+    "answer" : "a"
+
+}
+
+]
